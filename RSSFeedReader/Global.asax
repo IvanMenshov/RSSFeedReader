b869adaee88175e1be2e07156cda70a3b7e5909b
Ivan <38044561+IvanMenshov@users.noreply.github.com>
2020-02-13 16:26:01 +0300

    Add project files.

---
+++ RSSFeedReader/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RSSFeedReader.MvcApplication" Language="C#" %>
---
+++ RSSFeedReader/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RSSFeedReader.Startup))]
+namespace RSSFeedReader
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
