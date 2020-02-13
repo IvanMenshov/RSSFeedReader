8c6bf1c5e1a52b2a3bafcf123f6dabe2764ae1f9
Ivan <38044561+IvanMenshov@users.noreply.github.com>
2020-02-13 17:26:38 +0300

    Add logic and view to view RSS feed

---
+++ RSSFeedReader/Controllers/RSSFeedController.cs
@@ -0,0 +1,42 @@
+﻿using RSSFeedReader.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace RSSFeedReader.Controllers
+{
+    public class RSSFeedController : Controller
+    {
+        // GET: RSSFeed
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string RSSUrl)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            WebClient webClient = new WebClient();
+            string RSSData = webClient.DownloadString(RSSUrl);
+
+            XDocument xml = XDocument.Parse(RSSData);
+            var RSSFeedData = (from x in xml.Descendants("item")
+                               select new RSSFeed
+                               {
+                                   Title = (string)x.Element("title"),
+                                   Link = (string)x.Element("link"),
+                                   Description = (string)x.Element("description"),
+                                   PubDate = (string)x.Element("pubDate")
+                               });
+            ViewBag.RSSFeed = RSSFeedData;
+            ViewBag.URL = RSSUrl;
+
+            return View();
+        }
+    }
+}
---
+++ RSSFeedReader/Models/RSSFeed.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RSSFeedReader.Models
+{
+    public class RSSFeed
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public string PubDate { get; set; }
+    }
+}
